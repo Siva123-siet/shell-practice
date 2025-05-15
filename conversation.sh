449dbee2294247446e225114579a77a368683794
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-15 22:03:10 +0530

    commited

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "Trump :: Hi Musk, How are you doing"
-echo "Musk : Hi Trump, Iam good thank you. How about you"
-echo "Trump :: Iam too good.What about plan this weekend"
-echo "Musk :: Iam planning to have a trip to mars this weekend. Are you willing to come"
-echo "Trump: You carry on. We will go once you come back safely. Thanks for asking"
+Person1=Trump
+Person2=Musk
+echo "$Person1 :: Hi $Person2, How are you doing"
+echo "$Person2 : Hi $Person1, Iam good thank you. How about you"
+echo "$Person1 :: Iam too good.What about plan this weekend"
+echo "$Person2, :: Iam planning to have a trip to mars this weekend. Are you willing to come"
+echo "$Person1: You carry on. We will go once you come back safely. Thanks for asking"
