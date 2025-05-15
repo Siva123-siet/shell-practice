24f4d734cbe789a109f4e27f9d1240907886c2f6
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-15 16:10:26 +0530

    conversation created

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Trump :: Hi Musk, How are you doing"
+echo "Musk : Hi Trump, Iam good thank you. How about you"
+echo "Trump :: Iam too good.What about plan this weekend"
+echo "Musk :: Iam planning to have a trip to mars this weekend. Are you willing to come"
+echo "Trump: You carry on. We will go once you come back safely. Thanks for asking"
