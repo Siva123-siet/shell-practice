144ec53ac4113625dcf472a4bf571194a12f1fa4
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-17 17:52:08 +0530

    commited

--- install-command.sh
+++ install-command.sh
@@ -4,10 +4,18 @@ Userid=$(id -u)
 
 if [ $Userid -ne 0 ]
 then
-    echo "Please run script with root access:"
+    echo "Error: Please run script with root access:"
     exit 1
 else
     echo "You are running with root access"
 fi
 
-dnf install mysql
+dnf install.mysql
+
+if [ $? -eq 0]
+then
+    echo " Installing mysql"
+else
+    echo "Not installing mysql"
+    exit 1
+fi 
