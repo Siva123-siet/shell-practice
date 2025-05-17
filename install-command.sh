6f8a7725d9125aab6968cf0a5cd99fc5defe7352
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-17 18:32:15 +0530

    commited

--- install-command.sh
+++ install-command.sh
@@ -9,12 +9,17 @@ then
 else
     echo "You are running with root access"
 fi
-
-dnf install mysql
-if [ $? -eq 0 ]
-then
-    echo " Installing mysql"
+dnf list installed nginx
+if [ $? -ne 0 ]
+then   
+    dnf install nginx
+    if [ $? -eq 0 ]
+    then
+        echo " Installing nginx"
+    else
+        echo "Not installing nginx"
+        exit 1
+    fi 
 else
-    echo "Not installing mysql"
-    exit 1
-fi 
+    echo "Nginx already installed...Nothing to do"
+fi
