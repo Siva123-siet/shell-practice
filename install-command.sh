506d8b7dc96a11e2930c50a9f9c8562118b035e7
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-17 17:21:04 +0530

    commited

--- install-command.sh
+++ install-command.sh
@@ -4,7 +4,7 @@ Userid=$(id -u)
 
 if [ $Userid -ne 0 ]
 then
-    echo "Error: Please run script with root access:"
+    echo "Please run script with root access:"
 else
     echo "You are running with root access"
 fi
