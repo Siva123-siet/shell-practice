3795a16675e931ce4c78241f49cc5a6d0efca73f
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-17 13:16:56 +0530

    commited

---
+++ conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Number=$1
+
+-gt --> greater than
+-lt --> less than
+-eq --> equal
+-ne --> not equal
+
+if [$Number -lt 10]
+then
+    echo "Given Number $Number is less than 10"
+else
+    echo "Given Number $Number is not less than 10"
+fi
