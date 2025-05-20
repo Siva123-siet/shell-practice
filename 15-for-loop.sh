7c6c8eb4324edc99730e9ae0ad70f9f8c30c6675
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-20 18:22:29 +0530

    commited

--- 15-for-loop.sh
+++ 15-for-loop.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for i in {1..100}
-do
-   echo $i
+#for i in {1..100}
+#do
+ #  echo $i
+#done
+
+PACKAGES=("mysql","python","nginx")
+for package in ${PACKAGES[@]}
+do 
+   echo $package
 done
