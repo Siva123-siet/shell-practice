267b07c4490cc391cad0cd46ae30fb75308d30bb
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-20 15:43:43 +0530

    commited

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 Userid=$(id -u)
-
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 if [ $Userid -ne 0 ]
 then
-    echo "Error: Please run script with root access:"
+    echo -e "$R Error:Please run script with root access $N"
     exit 1
 else
     echo "You are running with root access"
@@ -13,17 +16,18 @@ VALIDATE()
 {
 if [ $1 -eq 0 ]
 then
-    echo " Installing $2"
+    echo -e "Installing $2 is ...$G Success $N"
 else
-    echo "Not installing $2"
+    echo -e "Installing $2 is ...$R FAILURE $N"
     exit 1
 fi 
 }
 dnf list installed nginx
 if [ $? -ne 0 ]
-then   
-    dnf install nginx
+then 
+    echo -e "Nginx is not installed...$G going to install it $N" 
+    dnf install nginx -y
     VALIDATE $? "Nginx"
 else
-    echo "Nginx already installed...Nothing to do"
+    echo -e "Nothing to do Nginx...$Y already installed $N"
 fi
